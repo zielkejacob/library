92951e2bee8ab0e722f3797f66f615dff291759c
zielkejacob <123435271+zielkejacob@users.noreply.github.com>
2023-02-26 20:58:42 -0600

    Create book constructor

---
+++ js/app.js
@@ -0,0 +1,22 @@
+let myLibrary = [];
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+Book.prototype.info = function () {
+  return `${this.title}, by ${this.author}, ${this.pages} pages, ${this.read}`;
+};
+
+const theHobbit = new Book(
+  "The Hobbit",
+  "J.R.R Tolkien",
+  "295",
+  "not read yet"
+);
+
+console.log(theHobbit.info());
+
+function addBookToLibrary() {}
